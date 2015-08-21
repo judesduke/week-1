1dad936fb2f2ab6ad3dbf41d503b14a974aaa733
judesduke <judes.duke@gmail.com>
2015-08-21 17:00:07 +0700

    three function

--- Lab/Lab1-2/Program.cs
+++ Lab/Lab1-2/Program.cs
@@ -10,34 +10,59 @@ namespace Lab1_2
     {
         static void Main(string[] args)
         {
+
+            input();
+
+            Console.ReadKey();
+
+        }
+
+        public static void input()
+        {
+
+            int x;
+            Console.Write("input size of number = ");
+            x = Convert.ToInt32(Console.ReadLine());
+            int[] y = new int[x];
+            for (int i = 0; i < y.Length; i++)
+            {
+                Console.Write("input number = ");
+                y[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            process(y);
+
+        }
+        public static void output(int[] x)
+        {
+            Console.Write("result = ");
+            for (int i = 0; i != x.Length; i++)
+            {
+                Console.Write(x[i]);
+                Console.Write(" ");
+            }
+
+        }
+        public static void process(int[] x)
+        {
+
             bool flag = true;
-
-            // input
-            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
-
-            // process
             while (flag)
             {
                 flag = false;
-                for (int i = 0; i != input.Length - 1; i++)
+                for (int i = 0; i != x.Length - 1; i++)
                 {
-                    if (input[i] > input[i + 1])
+                    if (x[i] > x[i + 1])
                     {
-                        int temp = input[i];
-                        input[i] = input[i + 1];
-                        input[i + 1] = temp;
+                        int temp = x[i];
+                        x[i] = x[i + 1];
+                        x[i + 1] = temp;
                         flag = true;
                     }
                 }
             }
-
-            // output
-            for (int i = 0; i != input.Length; i++)
-            {
-                Console.Write(input[i]);
-                Console.Write(" ");
-            }
-            Console.ReadKey();
+            output(x);
         }
+
     }
 }
